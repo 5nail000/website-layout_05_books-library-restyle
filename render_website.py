87fe9ffd59afbe534c603f391c60e76e5873e4ed
5nail000 <105508594+5nail000@users.noreply.github.com>
2023-01-29 01:49:31 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,26 +1,35 @@
 import json
 from pathlib import Path
+from livereload import Server, shell
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-all_books_filename = Path.cwd()/'downloads'/'parsed_books_data.json'
-with open(all_books_filename, encoding='utf_8') as file:
-    all_books = json.load(file)
+def on_reload():
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+    all_books_filename = Path.cwd()/'downloads'/'parsed_books_data.json'
+    with open(all_books_filename, encoding='utf_8') as file:
+        all_books = json.load(file)
 
-template = env.get_template('template.html')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-rendered_page = template.render(
-    all_books=all_books.values()
-)
+    template = env.get_template('template.html')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        all_books=all_books.values()
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+on_reload()
+# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+# server.serve_forever()
+
+server = Server()
+server.watch('template.html', on_reload)
+server.serve(root='.')
