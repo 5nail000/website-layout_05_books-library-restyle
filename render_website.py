7ac9d6b57ec94df15cf75187135678b1ff6dd849
5nail000 <105508594+5nail000@users.noreply.github.com>
2023-04-14 01:42:07 +0300

    homework

--- render_website.py
+++ render_website.py
@@ -9,42 +9,34 @@ from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def parse_argparse():
+def setup_parser():
     parser = argparse.ArgumentParser(description="Веб интерфейс для навигации по данным сохранённой библиотеки")
-    parser.add_argument(
-        'data_folder', type=str, default='media', help='Папка с данными, по умолчанию "media"'
-        )
+    parser.add_argument('datafile', type=str, default='', help='Относительный путь к файлу с данными')
 
     try:
-        data_folder = parser.parse_args().data_folder
-        if os.path.isfile(data_folder):
-            filename = os.path.basename(data_folder)
-            dir_name = os.path.dirname(data_folder)
+        datafile = parser.parse_args().datafile
+        if os.path.isfile(datafile):
+            datafile_parsed = datafile
         else:
-            filename = 'parsed_books_data.json'
-            dir_name = data_folder
+            datafile_parsed = f'{datafile}{os.sep}parsed_books_data.json'
     except SystemExit:
-        filename = 'parsed_books_data.json'
-        dir_name = Path.cwd()/'media'
+        datafile_parsed = 'parsed_books_data.json'
 
-    return dir_name, filename
+    return datafile_parsed
 
 
-def on_reload(data_path, data_filename):
+def on_reload(data_filename):
 
-    full_file_path = f'{data_path}{os.sep}{data_filename}'
+    full_file_path = data_filename
     with open(full_file_path, encoding='utf_8') as file:
         books_descriptions = json.load(file)
 
-    columns = 2
     books_cards_per_page = 20
     total_pages = math.ceil(len(books_descriptions)/books_cards_per_page)
+    book_cards_by_pages = list(chunked(books_descriptions.values(), books_cards_per_page))
 
-    doubled_cards = list(chunked(books_descriptions.values(), columns))
-    book_cards_by_pages = list(chunked(doubled_cards, int(books_cards_per_page/columns)))
-
-    pages_folder = 'pages'
-    os.makedirs(pages_folder, exist_ok=True)
+    folder_path = 'pages'
+    os.makedirs(folder_path, exist_ok=True)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -66,8 +58,7 @@ def on_reload(data_path, data_filename):
                                         total_pages=range(total_pages),
                                         previous_btn=page_prev_disabled,
                                         next_btn=page_next_disabled,
-                                        prefix=prefix,
-                                        mediafolder=data_path
+                                        prefix=prefix
                                         )
         if page_num == 0:
             with open('index.html', 'w', encoding='utf8') as file:
@@ -81,20 +72,18 @@ def on_reload(data_path, data_filename):
                                         total_pages=range(total_pages),
                                         previous_btn=page_prev_disabled,
                                         next_btn=page_next_disabled,
-                                        prefix=prefix,
-                                        mediafolder=data_path
+                                        prefix=prefix
                                         )
 
-        with open(Path.cwd()/pages_folder/f'index{page_num+1}.html', 'w', encoding='utf8') as file:
+        with open(Path.cwd()/folder_path/f'index{page_num+1}.html', 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
 
 if __name__ == '__main__':
 
-    data_path, data_filename = parse_argparse()
-    print(f'loaded data from: {data_path}{os.sep}{data_filename}')
-    on_reload(data_path, data_filename)
+    data_filename = setup_parser()
+    on_reload(data_filename)
 
     server = Server()
-    server.watch('template.html', on_reload(data_path, data_filename))
+    server.watch('template.html', lambda: on_reload(data_filename))
     server.serve(root='.')
