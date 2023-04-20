55cbc18bce9069ea053a747618622d2930c76ff5
5nail000 <105508594+5nail000@users.noreply.github.com>
2023-04-20 14:45:44 +0300

    cuts

--- render_website.py
+++ render_website.py
@@ -40,29 +40,12 @@ def on_reload(data_filename):
         page_prev_disabled = 'disabled' if page_num == 0 else ''
         page_next_disabled = 'disabled' if page_num == (total_pages-1) else ''
 
-        # Render Main Page
-        prefix = ''
-        rendered_page = template.render(
-                                        books_at_page=book_cards_by_pages[page_num],
-                                        current_page=page_num,
-                                        total_pages=range(total_pages),
-                                        previous_btn=page_prev_disabled,
-                                        next_btn=page_next_disabled,
-                                        prefix=prefix
-                                        )
-        if page_num == 0:
-            with open('index.html', 'w', encoding='utf8') as file:
-                file.write(rendered_page)
-
-        # Render /pages/
-        prefix = '../'
         rendered_page = template.render(
                                         books_at_page=book_cards_by_pages[page_num],
                                         current_page=page_num+1,
                                         total_pages=range(total_pages),
                                         previous_btn=page_prev_disabled,
-                                        next_btn=page_next_disabled,
-                                        prefix=prefix
+                                        next_btn=page_next_disabled
                                         )
 
         with open(Path.cwd()/folder_path/f'index{page_num+1}.html', 'w', encoding='utf8') as file:
